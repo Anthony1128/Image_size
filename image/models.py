c42800559fb7bddaef410e60613df441790c456d
Anthony <anthony@iMac-Anthony.local>
2020-09-03 18:26:25 +0300

    first

---
+++ image/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import os
+
+
+class Image(models.Model):
+    file = models.ImageField(upload_to='origin')
+    width = file.width_field
+    height = file.height_field
+
+    def __str__(self):
+        filename = os.path.basename(self.file.name)
+        return filename
