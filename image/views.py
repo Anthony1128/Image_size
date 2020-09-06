c618d74f0c28ea649987842c1ce61b6084b85105
Anthony <anthony@iMac-Anthony.local>
2020-09-06 18:01:39 +0300

    list shown

--- image/views.py
+++ image/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Image
+import os
 
-# Create your views here.
+
+def index(request):
+    images = Image.objects.all()
+    context = {'images': images}
+    return render(request, 'image/index.html', context)
+
+
+def one_image(request, image_id):
+    image = Image.objects.get(id=image_id)
+    context = {'image': image}
+    return render(request, 'image/image.html', context)
